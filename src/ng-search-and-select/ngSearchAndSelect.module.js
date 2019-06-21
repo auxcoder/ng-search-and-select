29458987c2e51f5bf1766242fe3c41bf1692a42e
Pavel Duran <auxcoder@gmail.com>
2019-06-21 11:31:28 -0400

    Annotate compoenent dependencies

--- src/ng-search-and-select/ngSearchAndSelect.module.js
+++ src/ng-search-and-select/ngSearchAndSelect.module.js
@@ -293,6 +293,7 @@
 			return $sanitize(substringOne + boldSubstring + substringThree);
 		}
   }
+	SearchSelectController.$inject = ['$scope', '$sanitize', '$document'];
 
   //////////////////////////////////////////////////////////////////////////////
 
